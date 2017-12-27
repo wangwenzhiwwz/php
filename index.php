3a42ad6015c8e16f7c528fea7e9fdceb7d04f7aa
小文之 <33089305+wangwenzhiwwz@users.noreply.github.com>
2017-12-27 18:01:26 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+//1写数据
+
+$data=array(
+	array("id"=>"1","name"=>"王海1","age"=>"20","classed"=>"1本科"),
+	array("id"=>"2","name"=>"王海12","age"=>"20","classed"=>"1本科"),
+	array("id"=>"3","name"=>"王海13","age"=>"20","classed"=>"1本科"),
+	array("id"=>"4","name"=>"王海14","age"=>"20","classed"=>"1本科"),
+	array("id"=>"5","name"=>"王海15","age"=>"20","classed"=>"1本科"),
+	array("id"=>"6","name"=>"王海16","age"=>"20","classed"=>"1本科"),
+	array("id"=>"7","name"=>"王海17","age"=>"20","classed"=>"1本科"),
+	array("id"=>"8","name"=>"王海18","age"=>"20","classed"=>"1本科"),
+	array("id"=>"9","name"=>"王海19","age"=>"20","classed"=>"1本科"),
+	array("id"=>"0","name"=>"王海10","age"=>"20","classed"=>"1本科"),
+	array("id"=>"9","name"=>"王海11","age"=>"20","classed"=>"1本科"),
+	array("id"=>"10","name"=>"王海12","age"=>"20","classed"=>"1本科"),
+	array("id"=>"11","name"=>"王海13","age"=>"20","classed"=>"1本科"),
+	array("id"=>"12","name"=>"王海14","age"=>"20","classed"=>"1本科"),
+	array("id"=>"13","name"=>"王海15","age"=>"20","classed"=>"1本科"),
+	array("id"=>"14","name"=>"王海16","age"=>"20","classed"=>"1本科"),
+	array("id"=>"15","name"=>"王海17","age"=>"20","classed"=>"1本科"),
+	array("id"=>"16","name"=>"王海18","age"=>"20","classed"=>"1本科"),
+	array("id"=>"17","name"=>"王海19","age"=>"20","classed"=>"1本科"),
+	array("id"=>"18","name"=>"王海10","age"=>"20","classed"=>"1本科"),
+	array("id"=>"19","name"=>"王海11","age"=>"20","classed"=>"1本科"),
+	array("id"=>"20","name"=>"王海12","age"=>"20","classed"=>"1本科")
+);
+
+//转换层json对象
+
+echo  json_encode($data);
+
+
+?>
